a1b3e3fdd9a0b68b3bf94f3003880741a38ad074
Duy Le <dle@walmartlabs.com>
2016-07-08 15:20:34 -0700

    remove redirect

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ var express = require('express')
 var app = express()
 const PORT = process.env.PORT || 3000;
 
-app.use( function(req, resp, next){
-  if (req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  }else{
-    resp.redirect('http://'+ req.hostname + req.url);
-  }
-})
+// app.use( function(req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'http'){
+//     next();
+//   }else{
+//     var x = 'http://'+ req.hostname + ':' + PORT + req.url;
+//     console.log(x)
+//     res.redirect(x);
+//   }
+// })
 
 app.use( express.static('public') )
 
