15d0e2f598b579734db80549c68086641b76ee73
Duy Le <dle@walmartlabs.com>
2016-07-08 15:46:32 -0700

    change http

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ var express = require('express')
 var app = express()
 const PORT = process.env.PORT || 3000;
 
-// app.use( function(req, res, next){
-//   if (req.headers['x-forwarded-proto'] === 'http'){
-//     next();
-//   }else{
-//     var x = 'http://'+ req.hostname + ':' + PORT + req.url;
-//     console.log(x)
-//     res.redirect(x);
-//   }
-// })
+app.use( function(req, res, next){
+  var proto = req.headers['x-forwarded-proto']
+  if (!proto){
+    next();
+  }else{
+    if (proto === 'http'){
+      next();
+    }else{
+      res.redirect('http://'+ req.hostname + req.url);
+    }
+  }
+})
 
 
 app.use( express.static('public') )
